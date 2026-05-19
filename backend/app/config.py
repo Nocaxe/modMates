86bfea1d1950623635cc3b03a49ea3867225b0b5
Nocaxe <123151145+Nocaxe@users.noreply.github.com>
2026-05-19 23:32:26 +0800

    Create settings instance

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,14 @@
 '''
 Configuration settings and env variables
 '''
+
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    database_url: str
+    
+    class Config:
+        env_file = '.env'
+
+# Instance used across the app
+settings = Settings()
