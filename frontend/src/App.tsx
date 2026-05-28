a75b0231d0bf42074e6f94c6bf79a1d888563eb4
Nocaxe <123151145+Nocaxe@users.noreply.github.com>
2026-05-28 15:30:43 +0800

    Create app with authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-export default function App() {
+import { AuthProvider, useAuth } from "./contexts/AuthContext"
+import { AuthForm } from "./components/AuthForm"
+
+
+function AppContent() {
+  const { session, loading, signOut } = useAuth()
+
+  if (loading) return <div>Loading...</div>
+  if (!session) return <AuthForm/>
 
   return (
-    <>
-        <div>
-          <h1>Get started</h1>
-        </div>
-    </>
+    <div>
+      <p>Logged in as {session.user.email}</p>
+      <button onClick={signOut}>Sign out</button>
+      {/* TODO: rest of the app here */}
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <AppContent/>
+    </AuthProvider>
   )
 }
