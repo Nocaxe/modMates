fca4cd46453407b60327f47dc1ba07da88cffeeb
Nocaxe <123151145+Nocaxe@users.noreply.github.com>
2026-06-03 00:45:14 +0800

    Added navigation to the header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -12,14 +12,20 @@ export function Header() {
 
     return (
         <div className="flex flex-row gap-4 justify-center bg-gray-800 p-4 rounded">
+            <button onClick={() => void navigate('/optimiser')} className="text-white py-2 px-4 rounded hover:bg-gray-700">
+                Optimiser
+            </button>
+            <button onClick={() => void navigate('/profile')} className="text-white py-2 px-4 rounded hover:bg-gray-700">
+                Profile
+            </button>
             {session && <>
-                <p className="text-white py-2">Logged in as {session.user.email}</p>
+                <p className="text-white py-2 px-4">Logged in as {session.user.email}</p>
                 <button onClick={() => void onSignOut()} className="bg-red-800 text-white py-2 px-4 rounded hover:bg-red-600">
                     Sign out
                 </button>
             </>}
             {!session && <>
-                <p className="text-white py-2">Not logged in</p>
+                <p className="text-white py-2 px-4">Not logged in</p>
                 <button onClick={() => void navigate('/')} className="bg-red-800 text-white py-2 px-4 rounded hover:bg-red-600">
                     Log in
                 </button>
