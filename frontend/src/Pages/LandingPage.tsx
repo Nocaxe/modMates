33c82247ebbf0261e9463f13acb7ca5afb95841a
Nocaxe <123151145+Nocaxe@users.noreply.github.com>
2026-06-02 13:32:48 +0800

    Implement redirect if user is already logged in

--- frontend/src/Pages/LandingPage.tsx
+++ frontend/src/Pages/LandingPage.tsx
@@ -1,15 +1,18 @@
 import { useNavigate } from "react-router-dom"
 import { useAuth } from "../contexts/AuthContext"
 import { AuthForm } from "../components/AuthForm"
+import { useEffect } from "react"
 
 export default function LandingPage() {
   const { session, loading } = useAuth() as { session: unknown, loading: boolean }
   const navigate = useNavigate()
 
-  function handleAuthSuccess() {
-    void navigate('/optimiser')
-  }
+  useEffect(() => {
+    if (!loading && session) {
+        void navigate('/optimiser', { replace: true })
+    }
+  }, [loading, session, navigate])
 
   if (loading) return <div>Loading...</div>
-  if (!session) return <AuthForm onSuccess={handleAuthSuccess}/>
+  if (!session) return <AuthForm onSuccess={() => void navigate('/optimiser')}/>
 }
