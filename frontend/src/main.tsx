c4e962957dad68e0ef146b3f5c66f38a679258f3
Nocaxe <123151145+Nocaxe@users.noreply.github.com>
2026-06-02 10:49:33 +0800

    Create router and link to app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import { AuthProvider } from "./contexts/AuthContext"
+import LandingPage from './Pages/LandingPage.tsx'
+import OptimiserPage from './Pages/OptimiserPage.tsx'
+import ProfilePage from './Pages/ProfilePage.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <LandingPage /> },
+      { path: 'optimiser', element: <OptimiserPage /> },
+      { path: 'profile', element: <ProfilePage /> }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
