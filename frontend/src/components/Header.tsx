d71f0965b99f71cb9878023ca8fb8c36c38e4ef2
Nocaxe <123151145+Nocaxe@users.noreply.github.com>
2026-06-02 11:45:53 +0800

    Make signout navigate to landing page

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,21 @@
+import { useNavigate } from "react-router-dom"
 import { useAuth } from "../contexts/AuthContext"
 
 export function Header() {
     const { session, signOut } = useAuth()
+    const navigate = useNavigate()
 
     if (!session) return null
+
+    function onSignOut() {
+        void signOut()
+        void navigate('/')
+    }
 
     return (
         <div className="flex flex-row gap-4 justify-center bg-gray-800 p-4 rounded">
         <p className="text-white py-2">Logged in as {session.user.email}</p>
-        <button onClick={() => void signOut()} className="bg-red-800 text-white py-2 px-4 rounded hover:bg-red-600">
+        <button onClick={onSignOut} className="bg-red-800 text-white py-2 px-4 rounded hover:bg-red-600">
             Sign out
         </button>
         </div>
