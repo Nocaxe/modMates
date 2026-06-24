36abc616acd974b13dde2da1c582e67e6a1c183e
Nocaxe <123151145+Nocaxe@users.noreply.github.com>
2026-06-24 09:59:11 +0800

    Add the new bottom panel to the optimiser page

--- frontend/src/Pages/OptimiserPage.tsx
+++ frontend/src/Pages/OptimiserPage.tsx
@@ -1,11 +1,32 @@
-import { NUSMods } from "../components/NUSMods"
-import TimetableUI from "../components/Timetable"
+import { useState } from "react";
+import TimetableUI from "../components/Timetable";
+import { BottomPanel } from "../components/BottomPanel";
 
 export default function OptimiserPage() {
-    return (
-        <>
-            <TimetableUI />
-            <NUSMods />
-        </>
-    )
+  const [constraintPayload, setConstraintPayload] = useState<object[]>([]);
+
+  // TODO: Replace sel and locked with actual data
+  {
+    /*
+  async function handleOptimise() {
+    const body = {
+        sel: (selected state),
+        locked: (locked state),
+        constraints: constraintPayload,
+    };
+    const res = await fetch('/api/optimise', {
+        method:'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+    });
+  }
+  */
+  }
+
+  return (
+    <div className="flex flex-col gap-4">
+      <TimetableUI />
+      <BottomPanel onConstraintsChange={setConstraintPayload} />
+    </div>
+  );
 }
